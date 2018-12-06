a2b2e0bbadc25897c46127d0d905281209b62c37
Atsushi <atsushi@moongift.jp>
2018-12-06 13:49:51 +0900

    Update

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const { promisify } = require('util');
+const { exec } = require('child_process')
+
 setInterval(async () => {
-  console.log(true);
+  const {tempOut, tempError} = await promisify(exec)('vcgencmd measure_temp');
+  const temperature = parseFloat(tempOut.replace(/temp=([0-9\.]*)'C/, '$1'));
+  
+  const {clockOut, clockError} = await promisify(exec)('vcgencmd measure_clock arm');
+  const clock = parseInt(clockOut.replace(/frequency.*=([0-9\.]*)$/, '$1'));
+  
+  const {voltOut, voltError} = await promisify(exec)('vcgencmd measure_volts');
+  const volt = parseFloat(voltOut.replace(/volt=([0-9\.]*)V$/, '$1'));
+  
+  console.log(`temperature: ${temperature} clock: ${clock} volt: ${volt}`)
 }, 5000);
