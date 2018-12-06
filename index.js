5a4dc8454c84327ed0f9971d40c98ead3372a89f
Atsushi <atsushi@moongift.jp>
2018-12-06 14:06:36 +0900

    Update

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ setInterval(async () => {
   const temperature = parseFloat(tempOut.stdout.replace(/temp=([0-9\.]*)'C/, '$1'));
   
   const clockOut = await promisify(exec)('vcgencmd measure_clock arm');
-  console.log('clockOut', clockOut);
   const clock = parseInt(clockOut.stdout.replace(/frequency.*=([0-9\.]*)/, '$1'));
   
   const voltOut = await promisify(exec)('vcgencmd measure_volts');
   const volt = parseFloat(voltOut.stdout.replace(/volt=([0-9\.]*)V/, '$1'));
   
+  const memOut = await promisify(exec)('free');
+  console.log(memOut.split(/\s/));
   console.log(`temperature: ${temperature} clock: ${clock} volt: ${volt}`)
 }, 5000);
