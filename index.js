eadad7ae98277638c700b662675ead67bd7048b6
Atsushi <atsushi@moongift.jp>
2018-12-06 15:30:19 +0900

    Update

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const { promisify } = require('util');
 const { exec } = require('child_process')
 
 setInterval(async () => {
+  // CPU温度
   const tempOut = await promisify(exec)('vcgencmd measure_temp');
   const temperature = parseFloat(tempOut.stdout.replace(/temp=([0-9\.]*)'C/, '$1'));
   
+  // CPU
   const clockOut = await promisify(exec)('vcgencmd measure_clock arm');
   const clock = parseInt(clockOut.stdout.replace(/frequency.*=([0-9\.]*)/, '$1'));
   
+  // 電圧
   const voltOut = await promisify(exec)('vcgencmd measure_volts');
   const volt = parseFloat(voltOut.stdout.replace(/volt=([0-9\.]*)V/, '$1'));
   
+  // メモリー
   const memOut = await promisify(exec)('free');
   const lines = memOut.stdout.split(/\r\n|\r|\n/);
   let params = lines[1].split(/\s+/);
@@ -28,5 +32,5 @@ setInterval(async () => {
     used: params[2],
     free: params[3]
   }
-  console.log(`temperature: ${temperature} clock: ${clock} volt: ${volt} memory: ${memory} swap: ${swap}`);
+  console.log(`temperature: ${temperature} clock: ${clock} volt: ${volt} memory: ${JSON.stringify(memory)} swap: ${JSON.stringify(swap)}`);
 }, 5000);
